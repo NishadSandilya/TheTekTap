ad0634aed2dbfedb967fe5075939109021e09e55
Nishad Sandilya <67221201+NishadSandilya@users.noreply.github.com>
2020-12-02 20:19:45 +0530

    update 4.0

---
+++ Javascript/script.js
@@ -0,0 +1,171 @@
+var btn1 = document.getElementById('b1');
+var btn2 = document.getElementById('b2');
+var btn3 = document.getElementById('b3');
+var btn4 = document.getElementById('b4');
+var btn5 = document.getElementById('b5');
+var btn6 = document.getElementById('b6');
+var btn7 = document.getElementById('b7');
+var btn8 = document.getElementById('b8');
+var btn9 = document.getElementById('b9');
+var btn10 = document.getElementById('b10');
+var btn11 = document.getElementById('b11');
+var btn12 = document.getElementById('b12');
+var btn13 = document.getElementById('b13');
+var btn14 = document.getElementById('b14');
+var btn15 = document.getElementById('b15');
+var btn16 = document.getElementById('b16');
+var btn17 = document.getElementById('b17');
+var btn18 = document.getElementById('b18');
+var btn19 = document.getElementById('b19');
+var btn20 = document.getElementById('b20');
+var ptn1 = document.getElementById('p1');
+var ptn2 = document.getElementById('p2');
+var ptn3 = document.getElementById('p3');
+var ptn4 = document.getElementById('p4');
+var ptn5 = document.getElementById('p5');
+var ptn6 = document.getElementById('p6');
+var ptn7 = document.getElementById('p7');
+var ptn8 = document.getElementById('p8');
+var ptn9 = document.getElementById('p9');
+var ptn10 = document.getElementById('p10');
+var ptn11 = document.getElementById('p11');
+var ptn12 = document.getElementById('p12');
+var ptn13 = document.getElementById('p13');
+var ptn14 = document.getElementById('p14');
+var ptn15 = document.getElementById('p15');
+var ptn16 = document.getElementById('p16');
+var ptn17 = document.getElementById('p17');
+var ptn18 = document.getElementById('p18');
+var ptn19 = document.getElementById('p19');
+var ptn20 = document.getElementById('p20');
+var cont = document.querySelector('.absoluto');
+var blr = document.querySelector('.blur');
+var cbtn = document.querySelector('.closebtn');
+var allclr = document.querySelector('.absoluto img')
+var mainfn = () => {
+    btn1.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn10.classList.toggle('desk-active')
+    })
+    btn2.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn11.classList.toggle('desk-active')
+    })
+    btn3.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn12.classList.toggle('desk-active')
+    })
+    btn4.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn9.classList.toggle('desk-active')
+    })
+    btn5.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn13.classList.toggle('desk-active')
+    })
+    btn6.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn14.classList.toggle('desk-active')
+    })
+    btn7.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn18.classList.toggle('desk-active')
+    })
+    btn8.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn6.classList.toggle('desk-active')
+    })
+    btn9.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn1.classList.toggle('desk-active')
+    })
+    btn10.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn2.classList.toggle('desk-active')
+    })
+    btn11.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn4.classList.toggle('desk-active')
+    })
+    btn12.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn3.classList.toggle('desk-active')
+    })
+    btn13.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn5.classList.toggle('desk-active')
+    })
+    btn14.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn7.classList.toggle('desk-active')
+    })
+    btn15.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn8.classList.toggle('desk-active')
+    })
+    btn16.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn19.classList.toggle('desk-active')
+    })
+    btn17.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn20.classList.toggle('desk-active')
+    })
+    btn18.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn17.classList.toggle('desk-active')
+    })
+    btn19.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn15.classList.toggle('desk-active')
+    })
+    btn20.addEventListener('click', () => {
+        blr.classList.toggle('desk-active')
+        cont.classList.toggle('desk-active')
+        ptn16.classList.toggle('desk-active')
+    })
+    cbtn.addEventListener('click', () => {
+        blr.classList.add('desk-active')
+        cont.classList.add('desk-active')
+        ptn1.classList.add('desk-active')
+        ptn2.classList.add('desk-active')
+        ptn3.classList.add('desk-active')
+        ptn4.classList.add('desk-active')
+        ptn5.classList.add('desk-active')
+        ptn6.classList.add('desk-active')
+        ptn7.classList.add('desk-active')
+        ptn8.classList.add('desk-active')
+        ptn9.classList.add('desk-active')
+        ptn10.classList.add('desk-active')
+        ptn11.classList.add('desk-active')
+        ptn12.classList.add('desk-active')
+        ptn13.classList.add('desk-active')
+        ptn14.classList.add('desk-active')
+        ptn15.classList.add('desk-active')
+        ptn16.classList.add('desk-active')
+        ptn17.classList.add('desk-active')
+        ptn18.classList.add('desk-active')
+        ptn19.classList.add('desk-active')
+        ptn20.classList.add('desk-active')
+    })
+}
+mainfn();
